276e724575267e7318721b280e166a59a481e1a5
Trevor Low <tlow94@uw.edu>
2020-04-25 21:10:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ def add_item(title, price, quantity=1)
   self.total += price * quantity
 end
 def apply_discount
-  binding.pry
+  #binding.pry
   self.total = (discount/100.to_f)*total).to_i
 end
 end
