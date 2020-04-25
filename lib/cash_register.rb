59ebaa07fd5aadc0c37b799bf2c80c9c270a066b
Trevor Low <tlow94@uw.edu>
2020-04-25 20:35:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,33 +1,30 @@
 require 'pry'
 class CashRegister
-    attr_accessor :discount, :total, :items, :last
-    def initialize(discount = 0)
-        @total = 0
-        @discount = discount
-        @items = []
+  attr_accessor :discount, :total, :item, :items, :last
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  def add_item (item, price, quantity = 1)
+    @total += price * quantity
+    quantity.times do
+      items << item
     end
-    def add_item (title, price, quantity = 1)
-        self.total += price * quantity
-        quantity.times do
-            items << title
-        end
-        self.last = price * quantity
-    end
-    def apply_discount
-        if discount != 0
-        self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-        #We have to change discount to a float and 100 to 100.0
-        #B.c once we divide by 100, it returns an integer, which is 0
-        #we want the float. Returns 640.0 before to_i (don't need to_i except to pass test)
-    "After the discount, the total comes to $#{self.total}."
-        else
-            "There is no discount to apply."
-    end
+    @last = price * quantity
+  end
+def apply_discount
+  #binding.pry
+  if discount != 0
+  @total = ((1.0-(discount/100.0).to_f) *total).to_i
+   #@total = (total * ((100.0 - discount.to_f)/100)).to_i
+   "After the discount, the total comes to $#{total}."
+ else
+   "There is no discount to apply."
 end
+end
+#binding.pry
 def void_last_transaction
-    self.total = self.total - self.last
+  @total = @total - @last
 end
-    
 end
-
-
