fc226e551528364e337719bb640b27600a843eb8
Trevor Low <tlow94@uw.edu>
2020-04-25 21:00:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,11 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discount
   def initialize(discount=0)
-  @discount = discount
-  @total = 0
+  self.discount = discount
+  self.total = 0
 end
 def add_item(title, price, quantity=1)
   binding.pry
   self.total += price * quantity
 end
 end
-add_item(title, price, quantity)
